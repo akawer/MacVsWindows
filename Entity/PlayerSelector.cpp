ba57e7b8e85c44ba78f4d455b35ee06b00a2811b
akawer <aaronkawer@gmail.com>
2014-04-12 15:30:14 -0500

    Modified PlayerSelector.cpp

--- Entity/PlayerSelector.cpp
+++ Entity/PlayerSelector.cpp
@@ -16,14 +16,31 @@ PlayerSelector::~PlayerSelector()
 vector<int> PlayerSelector::getControllerIds()
 {
   //TODO - Implement XBox Controller API class
+	joystick=getXboxControllerIds();
+	for (unsigned int j = 0; j < joystick.size(); ++j)
+		{
+			int i=joystick[j];
+		    if (sf::Joystick::isConnected(i))
+		    {
+
+		        float x    = sf::Joystick::getAxisPosition(i, sf::Joystick::X);		//TO DOOO!! make sure which button is what
+		        float y    = sf::Joystick::getAxisPosition(i, sf::Joystick::Y)    : 0;
+		    }
+		 }
+
   //TODO - implement this method
-  vector<int> v;
-  return v;
+	  vector<int> v;
+	  return v;
 }
 
 void PlayerSelector::update()
 {
   //TODO - Put logic here!
+	vector<int> a;
+	  a[0]=player1Choice;
+	  a[1]=player2Choice;
+	  Stage s(1,a);
+	  this->switchContext(s.getEntities());
 }
 
 
